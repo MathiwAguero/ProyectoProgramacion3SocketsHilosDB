44e7280043844d839b345ae0971cfdc2a8c42b46
mathe <mathiw.aguero.hernandez@est.una.ac.cr>
2025-10-31 21:07:08 -0600

    Solucionar errores

--- Protocol/src/main/java/hospital/Entities/Entities/Mensaje.java
+++ Protocol/src/main/java/hospital/Entities/Entities/Mensaje.java
@@ -1,18 +1,11 @@
 package hospital.Entities.Entities;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 
 public class Mensaje implements Serializable {
     private String mensaje;
 
     public Mensaje() {}
-
-    public Mensaje(String destinatarioId) {
-
-        this.mensaje = mensaje;
-
-    }
 
 
     public String getMensaje() {
