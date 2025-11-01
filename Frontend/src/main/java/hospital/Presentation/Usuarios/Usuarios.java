64f3803667d64f1b5985208b24716ed2e94f56ca
mathe <mathiw.aguero.hernandez@est.una.ac.cr>
2025-10-31 21:00:04 -0600

    Solucionar errores

--- Frontend/src/main/java/hospital/Presentation/Usuarios/Usuarios.java
+++ Frontend/src/main/java/hospital/Presentation/Usuarios/Usuarios.java
@@ -1,10 +1,34 @@
 package hospital.Presentation.Usuarios;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class Usuarios {
+public class Usuarios implements PropertyChangeListener {
     private JPanel panelUsuarios;
-    private JTable table1;
+    private JTable TablaUsuarios;
     private JButton enviarButton;
     private JButton recibirButton;
+
+    public Usuarios() {
+        enviarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        recibirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
